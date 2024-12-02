44103bc573ae0e0e700a87c49d055962b812396a
GiovaneDev <giovane.santosdm@gmail.com>
2024-12-02 21:17:30 +0000

    intent

--- app/src/main/java/com/example/fuelcalculator/PrecoActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/PrecoActivity.kt
@@ -7,21 +7,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
-class MainActivity : AppCompatActivity() {
+class PrecoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_preco)
 
-        val btniniciar = findViewById<Button>(R.id.btn_start)
+        val valor = intent.getFloatExtra("123", 0f)
 
-        btniniciar.setOnClickListener {
-            val intent = Intent(this, PrecoActivity::class.java)
-            intent.putExtra("123", "")
+        val edtvalor = findViewById<TextInputEditText>(R.id.edt_valor)
+        val button = findViewById<Button>(R.id.btn_next)
+
+        button.setOnClickListener {
+            val intent = Intent(this, ConsumoActivity::class.java)
+            intent.putExtra("123", valor)
             startActivity(intent)
         }
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
